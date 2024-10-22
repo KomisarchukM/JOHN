597f830d1c243c11fd55758adbc848ed9a5e3771
Komisarchuk Mark <snshn432@gmail.com>
2024-10-22 19:46:57 +0300

    experimented with back to top button

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let nav = document.querySelector(".nav")
+let bbt = document.querySelector(".bbt")
+window.onscroll = function() {scrollFunction()};
 function showMenu(){
     nav.style.display="flex"
 }
 function hideMenu(){
     nav.style.display="none"
+}
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btt.style.display = "inline-flex";
+  } else {
+    btt.style.display = "none";
+  }
 }
